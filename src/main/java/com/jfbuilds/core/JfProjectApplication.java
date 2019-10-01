f9059900158dbf785a1f09a42ff0d7fe1ff3246f
Jean-francois Nepton <jf@jfbuilds.com>
2019-10-01 00:58:23 -0400

    Create Maven Spring Project Based Structure

---
+++ src/main/java/com/jfbuilds/core/JfProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jfbuilds.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfbuilds/core/JfProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jfbuilds.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JfProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
